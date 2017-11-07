c67033104d40933f9e4ff8d4719a0ff97486ae77
drtelo <33463953+drtelo@users.noreply.github.com>
2017-11-08 06:55:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,12 +7,10 @@ foreach($sources as $source) {
 	$httpcode = shell_exec('curl -I --output /dev/null -w "%{http_code}" '.escapeshellarg($source));
 	$ext = pathinfo($source, PATHINFO_EXTENSION);
 	if($httpcode == 200) {
-		if($ext == 'zip') {
-			shell_exec('wget '.escapeshellarg($source).' -O master.zip; unzip -o master.zip; rm -f master.zip;');
-		} elseif($ext == 'gz') {
-			shell_exec('wget '.escapeshellarg($source).' -O master.tar.gz; tar xfz master.tar.gz --overwrite --unlink-first; rm -f master.tar.gz;');
-		} elseif($ext == 'tar') {
-			shell_exec('wget '.escapeshellarg($source).' -O master.tar.gz; tar xf master.tar --overwrite --unlink-first; rm -f master.tar;');
+		switch($ext) {
+			case 'gz': shell_exec('curl -o master.tar.gz '.escapeshellarg($source).'; tar xfz master.tar.gz --overwrite; rm -f master.tar.gz;'); break;
+			case 'tar': shell_exec('curl -o master.tar.gz '.escapeshellarg($source).'; tar xf master.tar --overwrite; rm -f master.tar;'); break;
+			case 'zip': shell_exec('curl -o master.zip '.escapeshellarg($source).'; unzip -o master.zip; rm -f master.zip;'); break;
 		}
 		if(empty(shell_exec('cat index.php | grep _ENV'))) { //index.php changed
 			header('Location: '.$_SERVER['REQUEST_URI']);
