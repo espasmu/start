1e14aa52b54cb3d7da43ccf076a48c1758c789e0
drtelo <33463953+drtelo@users.noreply.github.com>
2017-11-08 23:59:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,18 +4,16 @@ session_start();
 $sources = array_filter($_ENV, function($key) {return strpos($key, 'SOURCE_CODE') === 0;}, ARRAY_FILTER_USE_KEY);
 ksort($sources);
 foreach($sources as $source) {
-	$httpcode = shell_exec('curl -I --output /dev/null -w "%{http_code}" '.escapeshellarg($source));
-	$ext = pathinfo($source, PATHINFO_EXTENSION);
-	if($httpcode == 200) {
-		switch($ext) {
-			case 'gz': shell_exec('curl -o master.tar.gz '.escapeshellarg($source).'; tar xfz master.tar.gz --overwrite; rm -f master.tar.gz;'); break;
-			case 'tar': shell_exec('curl -o master.tar '.escapeshellarg($source).'; tar xf master.tar --overwrite; rm -f master.tar;'); break;
-			case 'zip': shell_exec('curl -o master.zip '.escapeshellarg($source).'; unzip -o master.zip; rm -f master.zip;'); break;
+	$content_type = shell_exec('curl -I --output /dev/null -w "%{content_type}" '.escapeshellarg($source));
+	if(preg_match('#application/.*(zip|tar)#i', $content_type)) {
+		switch(pathinfo($source, PATHINFO_EXTENSION)) {
+			case 'gz': shell_exec('curl -o source.tar.gz '.escapeshellarg($source).'; tar xfz source.tar.gz --overwrite; rm -f source.tar.gz;'); break;
+			case 'tar': shell_exec('curl -o source.tar '.escapeshellarg($source).'; tar xf source.tar --overwrite; rm -f source.tar;'); break;
+			case 'zip': shell_exec('curl -o source.zip '.escapeshellarg($source).'; unzip -o source.zip; rm -f source.zip;'); break;
 		}
-		if(empty(shell_exec('cat index.php | grep _ENV'))) { //index.php changed
+		if(empty(shell_exec('cat index.php | grep _ENV'))) { // index.php successfully changed
 			header('Location: '.$_SERVER['REQUEST_URI']);
 			exit;
-			break;
 		}
 	}
 }
